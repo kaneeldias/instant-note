9962daa8b8e6933769d95db7f62a81701c0d4f00
kaneeldias <kaneeldias@hotmail.com>
2023-05-29 19:28:19 +0530

    Update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ function HomePage() {
 		<div className="flex flex-row items-center justify-center">
 			<div className="flex flex-col items-left justify-center h-screen">
 				<div className="mb-10 flex flex-col items-left justify">
-					<h1 className="text-5xl font-bold">kNotex</h1>
+					<h1 className="text-5xl font-bold">kNote v 0.1.0</h1>
 					<p className="text-2xl font-light">take a note you fucking idiot</p>
 				</div>
 				<div className="flex flex-row items-stretch space-x-10">
