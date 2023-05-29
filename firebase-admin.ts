7ab7b956e315fe0a51dae1b0f2a34563c0078c06
kaneeldias <kaneeldias@hotmail.com>
2023-05-29 16:02:22 +0530

    Update PR workflow to use env variables

--- firebase-admin.ts
+++ firebase-admin.ts
@@ -5,8 +5,11 @@ const serviceAccount = require("./service-account.json");
 
 if (!getApps().length) {
 	admin.initializeApp({
-		credential: admin.credential.cert(serviceAccount),
-		databaseURL: "https://<your-project-id>.firebaseio.com"
+		credential: admin.credential.cert({
+			projectId: process.env.FIREBASE_PROJECT_ID,
+			clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+			privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n")
+		})
 	});
 }
 
