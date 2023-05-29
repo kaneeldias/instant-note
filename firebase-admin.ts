b14723d0695fbcb2192ce2a3f11117fa97e5d729
kaneeldias <kaneeldias@hotmail.com>
2023-05-29 16:06:58 +0530

    Remove service account import

--- firebase-admin.ts
+++ firebase-admin.ts
@@ -1,12 +1,10 @@
 import admin from "firebase-admin";
 import { getApps } from "firebase-admin/app";
 
-const serviceAccount = require("./service-account.json");
-
 if (!getApps().length) {
 	admin.initializeApp({
 		credential: admin.credential.cert({
-			projectId: process.env.FIREBASE_PROJECT_ID,
+			projectId: "tiktok",
 			clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
 			privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n")
 		})
