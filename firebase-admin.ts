7e6713303d88b49b76922705d14277f0772afbc6
kaneeldias <kaneeldias@hotmail.com>
2023-05-29 16:31:03 +0530

    Amend workflow

--- firebase-admin.ts
+++ firebase-admin.ts
@@ -4,7 +4,7 @@ import { getApps } from "firebase-admin/app";
 if (!getApps().length) {
 	admin.initializeApp({
 		credential: admin.credential.cert({
-			projectId: "tiktok",
+			projectId: process.env.FIREBASE_PROJECT_ID,
 			clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
 			privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n")
 		})
